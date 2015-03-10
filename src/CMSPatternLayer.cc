84588f4036b8b237c1d24c2aec94824e81314d78
Guillaume <g.baulieu@ipnl.in2p3.fr>
2015-03-10 12:26:25 +0100

    Remove 1 bit for the module Z and add it to the superstrip position (down to 8 strips wide) Do not use the segment in the barrel layers

--- src/CMSPatternLayer.cc
+++ src/CMSPatternLayer.cc
@@ -467,9 +467,9 @@ int CMSPatternLayer::getNbStripsInSegment(){
 
 int CMSPatternLayer::getSegmentCode(int layerID, int ladderID, int segmentID){
   if(layerID>7 && layerID<11)
-    return segmentID;
+    return segmentID/2;
   if(layerID>=5 && layerID<=7)
-    return segmentID/16;
+    return segmentID/32;
   if(ladderID<=8)
     return segmentID/16;
   return segmentID;
--- src/CMSPatternLayer.h
+++ src/CMSPatternLayer.h
@@ -32,14 +32,14 @@ using namespace std;
 
 class CMSPatternLayer : public PatternLayer{
  private:
-  static const short MOD_START_BIT = 11;
-  static const short PHI_START_BIT = 7;
+  static const short MOD_START_BIT = 12;
+  static const short PHI_START_BIT = 8;
   static const short STRIP_START_BIT = 1;
   static const short SEG_START_BIT = 0;
 
-  static const short MOD_MASK = 0x1F;
+  static const short MOD_MASK = 0xF;
   static const short PHI_MASK = 0xF;
-  static const short STRIP_MASK = 0x3F;
+  static const short STRIP_MASK = 0x7F;
   static const short SEG_MASK = 0x1;
 
   short binaryToGray(short num);
